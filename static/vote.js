262bcbfdf8d799bcfe12bd7e2642235174391ab9
Jan Düpmeier <j.duepmeier@googlemail.com>
2014-04-21 10:33:06 +0200

    now with sorting and better performance

--- static/vote.js
+++ static/vote.js
@@ -4,16 +4,16 @@
  * and open the template in the editor.
  */
 var vote = {
-    url: "server/api.php",
+    url: "http://vote.nsa-cloud.org/server/api.php",
     //url:"server/api.php",
     init: function() {
 	vote.getRandomImage();
 	vote.getAnswers();
-	
-	
+
+
 	document.addEventListener("keyup", function(event) {
 	    console.log(event);
-	    if (event.keyCode == 39 ) {
+	    if (event.keyCode == 39) {
 		vote.vote(0);
 	    }
 	    if (event.keyCode == 37) {
@@ -29,9 +29,9 @@ var vote = {
 		var img = document.createElement('img');
 		img.setAttribute('src', parsed.get[0].url);
 		img.setAttribute('alt', "picture");
-		
+
 		var picture = document.getElementById("picture");
-		
+
 		picture.innerHTML = "";
 		picture.setAttribute("data-id", parsed.get[0].id);
 		picture.appendChild(img);
@@ -44,18 +44,18 @@ var vote = {
     },
     getAnswers: function() {
 	ajax.asyncGet(vote.url + "?answers", function(xhr) {
-	   var parsed = JSON.parse(xhr.response);
-	   var elem = document.getElementById("answers");
-	      
-	      elem.innerHTML = "";
-	   parsed.answer.forEach(function(val) {
-	      var span = document.createElement('span');
-	      span.setAttribute('class', 'button');
-	      span.setAttribute('onclick', "vote.vote(" + val.id + ");");
-	      span.textContent = val.name;
-	      
-	      elem.appendChild(span);
-	   });
+	    var parsed = JSON.parse(xhr.response);
+	    var elem = document.getElementById("answers");
+
+	    elem.innerHTML = "";
+	    parsed.answer.forEach(function(val) {
+		var span = document.createElement('span');
+		span.setAttribute('class', 'button');
+		span.setAttribute('onclick', "vote.vote(" + val.id + ");");
+		span.textContent = val.name;
+
+		elem.appendChild(span);
+	    });
 	});
     },
     vote: function(id) {
@@ -64,132 +64,138 @@ var vote = {
 	    image: document.getElementById("picture").dataset.id,
 	    reply: id
 	};
-	
+
 	ajax.asyncPost(vote.url + "?vote", JSON.stringify(obj), function(xhr) {
-	   vote.setStatus(JSON.parse(xhr.response).status.replies[2]);
-	   vote.getRandomImage();
+	    vote.setStatus(JSON.parse(xhr.response).status.replies[2]);
+	    vote.getRandomImage();
 	});
     },
     setStatus: function(string) {
 	document.getElementById("status").textContent = string;
     },
+    columns: {
+	id: function(val) {
+	    var id = document.createElement('td');
+	    id.setAttribute('id', 'row' + val.id);
+	    id.textContent = val.id;
+	    return id;
+	},
+	img: function(val) {
+	    var picture = document.createElement('img');
+	    picture.setAttribute('height', "50px");
+	    picture.setAttribute('src', val.url);
+	    picture.setAttribute('alt', val.url);
+
+	    var ptd = document.createElement('td');
+	    ptd.setAttribute('class', 'colPtd');
+	    ptd.setAttribute('id', 'ptd' + val.id);
+	    ptd.appendChild(picture);
+	    return ptd;
+	},
+	up: function(val) {
+	    var td = document.createElement('td');
+	    td.setAttribute("id", "trElem" + val.id + "/" + 1);
+	    td.setAttribute("class", "trCol" + 1);
+	    return td;
+	},
+	down: function(val) {
+	    var td = document.createElement('td');
+	    td.setAttribute("id", "trElem" + val.id + "/" + 2);
+	    td.setAttribute("class", "trCol" + 2);
+	    return td;
+	},
+	av: function(val) {
+	    var av = document.createElement('td');
+	    av.setAttribute('id', "av" + val.id);
+	    return av;
+	}
+    },
     stats: function() {
 	ajax.asyncGet(vote.url + "?sum_votes", function(xhr) {
-	   document.getElementById("sumVotes").textContent = "Votes: " + JSON.parse(xhr.response).sum[0].votes;
+	    document.getElementById("sumVotes").textContent = "Votes: " + JSON.parse(xhr.response).sum[0].votes;
 	});
 	ajax.asyncGet(vote.url + "?stats", function(xhr) {
-	    
+
 	    var stats = JSON.parse(xhr.response).stats;
-	    
-	    var answers = JSON.parse(xhr.response).answer;
-	    
-	    //var elem = document.getElementById("headLine");
-	    //var elem2 = document.getElementById("tHead");
-	    
-	    /*answers.forEach(function(val) {
-		var th = document.createElement('th');
-		th.textContent = val.name;
-		elem.appendChild(th);
-		elem2.appendChild(th);
-	    });*/
-	    
-	    /**
-	     * only for this
-	     */
-	    //var av = document.createElement('th');
-	    //av.textContent = "Durschnitt";
-	    //elem.appendChild(av);
-	    //elem2.appendChild(av);
-	    
-	    
-	    var topVoted = null;
-	    var topHated = null;
-	    
+
 	    stats.forEach(function(val) {
-		
-		var tr = document.createElement('tr');
-		tr.setAttribute("id", "tr" + val.id);
-		
-		var id = document.createElement('td');
-		id.textContent = val.id;
-		tr.appendChild(id);
-		
-		var picture = document.createElement('img');
-		picture.setAttribute('height', "50px");
-		picture.setAttribute('src', val.url);
-		picture.setAttribute('alt', val.url);
-		
-		var ptd = document.createElement('td');
-		ptd.appendChild(picture);
-		tr.appendChild(ptd);
-		
-		answers.forEach(function(a) {
-		    var td = document.createElement('td');
-		    td.setAttribute("id", "trElem" +  val.id + "/" + a.id);
-		    td.setAttribute("class", "trCol" + a.id);
-		    tr.appendChild(td);
-		});
-		
-		if (document.getElementById('tr' + val.id)) {
-		    var d = document.getElementById('tr' + val.id);
-		    document.getElementById('tableBody').replaceChild(tr,d);
-	    } else {
-		document.getElementById('tableBody').appendChild(tr);
-	    }
-		
-		var avValue = 0;
-		
-		val.stats.forEach(function(a) {
-		    var e = null;
-		    while (!e) {
-		    e = document.getElementById('trElem' + a.image + "/" + a.reply);
-		}
-		    e.textContent = a.votes;
-		    if (a.reply == 2) {
-			avValue -= parseInt(a.votes);
-		    } else {
-			avValue += parseInt(a.votes);
-		    }
-		
-		});
-		
-		/**
-		 * only for this app
-		 */
-		var av = document.createElement("td");
-		av.textContent = avValue;
-		tr.appendChild(av);
-		
-		if (!topVoted) {
-		    topVoted = val;
-		    topVoted.av = avValue;
+
+		if (document.getElementById("row" + val.id)) {
+		    vote.updateStats(val);
 		} else {
-		    if (topVoted.av < avValue) {
-			topVoted = val;
-			topVoted.av = avValue;
-		    }
+
+		    var tr = document.createElement('tr');
+		    tr.setAttribute("id", "tr" + val.id);
+		    
+		    tr.appendChild(vote.columns.id(val));
+		    tr.appendChild(vote.columns.img(val));
+		    var up = vote.columns.up(val);
+		    tr.appendChild(up);
+		    var down = vote.columns.down(val);
+		    tr.appendChild(down);
+		    var av = vote.columns.av(val);
+		    tr.appendChild(av);
+
+		    document.getElementById('tableBody').appendChild(tr);
+		    
+		    vote.updateStats(val);
+
 		}
-		if (!topHated) {
-		    topHated = val;
-		    topVoted.av = avValue;
-		} else {
-		    if (topHated.av > avValue) {
-			topHated = val;
-			topHated.av = avValue;
-		    }
-		}
-	    
-	    var topV = document.getElementById('tr' + topVoted.id).cloneNode(true);
-	    var topH = document.getElementById('tr' + topHated.id).cloneNode(true);
-	    
-	    document.getElementById("top").innerHTML = "";
-	    
-	    document.getElementById("top").
-		    appendChild(topV);
-	    document.getElementById("top").
-		    appendChild(topH);
-		});
+	    });
+	    var topV = document.getElementById('tr' + vote.topVoted.id).cloneNode(true);
+	    var topH = document.getElementById('tr' + vote.topHated.id).cloneNode(true);
+		    document.getElementById("top").innerHTML = "";
+		    document.getElementById("top").
+			    appendChild(topV);
+		    document.getElementById("top").
+			    appendChild(topH);
+
 	});
+    },
+    updateStats: function(val) {
+	var up = document.getElementById('trElem' + val.id + "/" + 1);
+
+	var down = document.getElementById('trElem' + val.id + "/" + 2);
+	
+	var av = document.getElementById('av' + val.id);
+	var avValue = 0; 
+
+	val.stats.forEach(function(a) {
+
+	    if (a.reply == 1) {
+		up.textContent = a.votes;
+		avValue += parseInt(a.votes);
+	    }
+	    if (a.reply == 2) {
+		down.textContent = "-" + a.votes;
+		avValue -= parseInt(a.votes);
+	    }
+
+	    
+	});
+	    av.textContent = avValue;
+	    vote.checkTopVoted(val);
+    },
+    checkTopVoted: function(val) {
+	var current = parseInt(document.getElementById('av' + val.id).textContent);
+	if (!vote.topVoted) {
+	    vote.topVoted = val;
+	    vote.topVoted.av = current;
+	} else {
+	    if (vote.topVoted.av < current) {
+		vote.topVoted = val;
+		vote.topVoted.av = current;
+	    }
+	}
+	if (!vote.topHated) {
+	    vote.topHated = val;
+	    vote.topHated.av = current;
+	} else {
+	    if (vote.topHated.av > current) {
+		vote.topHated = val;
+		vote.topHated.av = current;
+	    }
+	}
     }
 };
 
